4fc231e54324f4b47375f0c45d770f04343576d1
Joowd <joowdx@pm.me>
2025-05-21 09:30:02 +0800

    Implement UserRole enum with HasLabel contract and add getLabel method

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -2,9 +2,20 @@
 
 namespace App\Enums;
 
-enum UserRole: string
+use Filament\Support\Contracts\HasLabel;
+
+enum UserRole: string implements HasLabel
 {
     case ADMIN = 'admin';
     case AGENTS = 'agents';
     case FRONTDESK = 'frontdesk';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::ADMIN => 'Administrator',
+            self::AGENTS => 'Agents',
+            self::FRONTDESK => 'Frontdesk',
+        };
+    }
 }
