8b5ad0377aa2a58a0192cb613ebb5e13a143d9e5
Joowd <joowdx@pm.me>
2025-05-21 10:01:32 +0800

    Add TextInput configuration to AppServiceProvider for maxLength setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        TextInput::configureUsing(fn (TextInput $component) => $component->maxLength(255));
+
         Select::configureUsing(fn (Select $component) => $component->native(false));
     }
 }
