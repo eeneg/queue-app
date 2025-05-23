b631a5463bae3f5f69c5b41cd1ec552805ce9cdb
Joowd <joowdx@pm.me>
2025-05-23 10:40:44 +0800

    Add ClearLeftover action to ListTickets page

--- app/Filament/Resources/TicketResource/Pages/ListTickets.php
+++ app/Filament/Resources/TicketResource/Pages/ListTickets.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Resources\TicketResource\Pages;
 
+use App\Enums\LogStatus;
 use App\Filament\Resources\TicketResource;
 use App\Models\Service;
+use App\Models\Ticket;
+use Exception;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Auth;
 
 class ListTickets extends ListRecords
 {
@@ -14,6 +18,45 @@ class ListTickets extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('cancel-leftovers')
+                ->requiresConfirmation()
+                ->modalDescription('Cancel all leftover tickets from previous days')
+                ->action(function (Actions\Action $component) {
+                    try {
+                        $component->beginDatabaseTransaction();
+
+                        Ticket::query()
+                            ->whereDate('created_at', '<', now())
+                            ->where(function ($query) {
+                                $query->whereDoesntHave('transaction')
+                                    ->orWhereHas('transaction', fn ($query) => $query->whereRelation('log', 'status', LogStatus::SKIPPED));
+                            })
+                            ->lazyById()
+                            ->each(function (Ticket $ticket) {
+                                $transaction = $ticket->transaction()->create([
+                                    'remarks' => 'Cancelled due to leftover',
+                                    'user_id' => Auth::id(),
+                                ]);
+
+                                $transaction->log()->create([
+                                    'status' => LogStatus::CANCELLED,
+                                    'user_id' => Auth::id(),
+                                ]);
+                            });
+
+                        $component->commitDatabaseTransaction();
+
+                        $component->successNotificationTitle('Leftover tickets cancelled successfully');
+
+                        $component->success();
+                    } catch (Exception) {
+                        $component->rollbackDatabaseTransaction();
+
+                        $component->failureNotificationTitle('Failed to cancel leftover tickets');
+
+                        $component->failure();
+                    }
+                }),
             Actions\CreateAction::make()
                 ->modalWidth('lg')
                 ->createAnother(false)
