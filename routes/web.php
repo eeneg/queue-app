6858f23b7f64479056f229c40cbbfc1f51d33363
Joowd <joowdx@pm.me>
2025-05-27 09:44:29 +0800

    Add API routes for services and tickets; remove routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,1 @@
 <?php
-
-use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\TicketController;
-use Illuminate\Support\Facades\Route;
-
-Route::resource('services', ServiceController::class)->only(['index', 'show']);
-
-Route::resource('tickets', TicketController::class)->only(['store', 'show']);
