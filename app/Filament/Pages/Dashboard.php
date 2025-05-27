8553d9885e6f6c8e83e2c8ba2890937b77b9d5ee
Joowd <joowdx@pm.me>
2025-05-27 10:07:12 +0800

    Add Dashboard page with counters and queue computations

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\Counter;
+use App\Models\Ticket;
+use Filament\Pages\Dashboard as Page;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Collection;
+use Livewire\Attributes\Computed;
+
+class Dashboard extends Page
+{
+    protected static string $view = 'filament.pages.dashboard';
+
+    protected ?string $maxContentWidth = 'screen-2xl';
+
+    public function getTitle(): string | Htmlable
+    {
+        return '';
+    }
+
+    #[Computed]
+    public function counters(): Collection
+    {
+        return Counter::query()
+            ->active()
+            ->occupied()
+            ->get();
+    }
+
+    #[Computed]
+    public function queue(): Collection
+    {
+        return Ticket::query()
+            ->whereDoesntHave('transaction')
+            ->get();
+    }
+
+}
